43584cf91d315f637be340aa17aa0022815cc798
Dashan Zhou <dashan.zhou@ta029393mac.tattsgroup.com>
2019-01-23 14:14:31 +1000

    adding extra twoway binding

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,13 +1,49 @@
 import React from 'react';
 
+class Contact extends React.Component {
 
-const Contact = () =>  {
+    state ={
+        name: 'Dashan'
+    }
+    
+    chnageName = (newName) => {
+        this.setState(
+            {
+                name:newName  
+            }
+        )
+    }
+    
+    chnageNamefromInput = (event) => {
+        this.setState(
+            {
+                name:event.target.value  
+            }
+        )
+    }
 
-    return (
-        <div>
-            <h1>Contact</h1>
-        </div>
-    );
+
+    render() {
+    
+        return (
+            <div className="App">
+                <br /><br/>
+    
+                <button onClick={() => this.chnageName('Awesome Dashan - :-(')}>
+                    Change State using A non function</button>
+                <button onClick={this.chnageName.bind(this, 'Awesome Dashan - :-)')}>
+                    Change State using bind function</button>
+                <br/>
+                <input type='text' onChange={this.chnageNamefromInput} 
+                    value={this.state.name}></input>
+                <br/>
+                Name: {this.state.name}
+                <br></br><br/>
+                <h1>Contact</h1>
+            </div>
+        );
+    }
+    
 }
 
 export default Contact;
