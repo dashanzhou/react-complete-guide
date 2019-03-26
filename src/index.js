d45443d4a671554f687bac3a6f52f2d8957d833c
Dashan Zhou <dashan.zhou@bpsoftware.net>
2019-03-26 16:49:01 +1000

    Adding decoration

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
+import { Provider as mProvider } from 'mobx-react';
 import { createStore } from 'redux';
 import reducer from './Store/Reducer';
+import BirdStore from './Store/BirdsStore';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const Root = (
+    <mProvider BirdStore={BirdStore}>
+        <App />
+    </mProvider>
+)
+
+//ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(Root, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
